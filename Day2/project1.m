51cb3408d4fdd851afe3f9a41cb26e13755f181c
Jack Corrette <Jack Corrette@DESKTOP-FN9RRNV>
2020-09-02 13:59:50 -0700

    first edit of file

---
+++ Day2/project1.m
@@ -0,0 +1,23 @@
+% Declare Variables
+c = -0.8;
+d = 0.165;
+nmax = 22;
+
+% ICs
+x = zeros(nmax,1);
+y = x;
+x(1) = 0.1;
+y(1) = 0.1;
+
+for k = 1:nmax-1
+   x(k+1) = x(k).^2  - y(k).^2 + c;
+   y(k+1) = 2.*x(k).*y(k) + d;
+end
+
+
+
+figure
+plot(x,y,'-ob','linewidth',1.5)
+xlabel('x','fontsize',12)
+ylabel('y','fontsize',12)
+title(['c = ',num2str(c),' d = ',num2str(d)])
